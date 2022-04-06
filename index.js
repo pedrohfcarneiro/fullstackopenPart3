932b0573eef1f5e7bc12c7c6183dd7b3234abc3f
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-04-06 16:27:19 -0300

    commit production build

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const path = require('path')
 const fs = require('fs')
 const cors = require('cors')
 
+
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let contacts = [
 	{
