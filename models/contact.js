8a86deed6dd45e13f85892fb4cb579e8fd66159b
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-04-07 15:45:29 -0300

    fix at toJSON formatting

--- models/contact.js
+++ models/contact.js
@@ -16,11 +16,12 @@ const contactSchema = new mongoose.Schema({
 	phone: String,
 })
 
-contactSchema.set('toJSON' {
+contactSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('Contact', noteSchema)
